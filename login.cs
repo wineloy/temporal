38a3298553f5468bd4b0542ace09770f101bd3d9
wineloy <wineloy@gmail.com>
2021-11-22 23:17:24 -0600

    Update login.cs

--- login.cs
+++ login.cs
@@ -1,1 +1,5 @@
-
+	public static string GetAuth(string username, string password)
+	{
+		var plainTextBytes = System.Text.Encoding.UTF8.GetBytes( username + ":" + password);
+		return "Basic " + Convert.ToBase64String(plainTextBytes);
+	}
